95d25b15ab1fddfde2f19a35cc7935a84b0b24ae
Andrew Burnes <apburnes@gmail.com>
2014-11-24 21:15:25 -0700

    Switch migartions to use readSql utility

--- migrations/1416543284643-buildGeodata.js
+++ migrations/1416543284643-buildGeodata.js
@@ -1,24 +1,20 @@
 'use strict';
 
 var query = require('../query');
+var readSql = require('../util/readSql');
 
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));
-
-var path = require('path');
-
-var sqlUp = path.join(__dirname, '../sqlMigrations/createGeodata.sql');
-var sqlDown = path.join(__dirname, '../sqlMigrations/dropGeodata.sql');
+var sqlUp = readSql(__dirname, '../sqlMigrations/createGeodata.sql');
+var sqlDown = readSql(__dirname, '../sqlMigrations/dropGeodata.sql');
 
 exports.up = function(success, error) {
-  fs.readFileAsync(sqlUp, 'utf-8')
+  sqlUp
     .then(query)
     .then(success)
     .catch(error);
 };
 
 exports.down = function(success, error) {
-  fs.readFileAsync(sqlDown, 'utf-8')
+  sqlDown
     .then(query)
     .then(success)
     .catch(error);
